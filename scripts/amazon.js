03adb756e0c7662506c39136e1d64832c13c39fd
Vrutti88 <vruttipatil1396@gmail.com>
2025-11-14 23:31:55 +0530

    cart-quantity

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -102,6 +102,16 @@ document.querySelectorAll('.add-to-cart-button').forEach((button)=>{
             quantity:1
           })
         }
+
+        let cartQuantity=0;
+
+        cart.forEach((item)=>{
+          cartQuantity+=item.quantity
+        })
+
+        document.querySelector('.cart-quantity').innerHTML=cartQuantity
+
+        console.log(cartQuantity)
         console.log(cart)
     })
 })
