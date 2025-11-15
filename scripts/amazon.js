41506bd47003536bf55941c389a663977b87e872
Vrutti88 <vruttipatil1396@gmail.com>
2025-11-15 23:38:47 +0530

    checkout

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -27,6 +27,7 @@
 import { cart } from '../data/cart.js'
 import { addToCart } from '../data/cart.js'
 import { products } from '../data/products.js'
+import { formatCurrency } from './utils/money.js'
 
 let productsHTML = '';
 
@@ -51,7 +52,7 @@ products.forEach((product) => {
           </div>
 
           <div class="product-price">
-            $${(product.priceCents / 100).toFixed(2)}
+            $${formatCurrency(product.priceCents)}
           </div>
 
           <div class="product-quantity-container">
