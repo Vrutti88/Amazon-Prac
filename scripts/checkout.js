e4f955b399fb1b0a7b19d660e1c3c28c6032b8f8
Vrutti88 <vruttipatil1396@gmail.com>
2025-11-16 14:35:01 +0530

    delete

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -15,7 +15,7 @@ cart.forEach((cartItem)=>{
         }
     })
     cartSummaryHTML+=`
-    <div class="cart-item-container">
+    <div class="cart-item-container-${matchingProduct.id}">
             <div class="delivery-date">
               Delivery date: Tuesday, June 21
             </div>
@@ -100,5 +100,8 @@ document.querySelectorAll('.delete-quantity-link').forEach((link)=>{
         const productId=link.dataset.productId
         removeFromCart(productId);
         console.log(cart)
+
+        const container=document.querySelector(`.cart-item-container-${productId}`)
+        container.remove();
     })
 })
